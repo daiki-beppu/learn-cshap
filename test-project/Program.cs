35e0c533b65d82a6ec2324c3aa03b548991a67e9
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-09-07 21:54:21 +0900

    add:

--- test-project/Program.cs
+++ test-project/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+// Console.WriteLine("Hello, C#!");
+
+Console.WriteLine('b');
+
+Console.WriteLine("Hello World");
+
+Console.WriteLine(123);
