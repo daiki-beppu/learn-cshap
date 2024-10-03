ff07dc5f9fe5500cac1c2237eebdce9d066adf49
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-10-03 12:35:53 +0900

    add:
    コーディング演習の追加

---
+++ hello-world/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, momochico!");
+
+string myName = "Daiki Beppu";
+myName = 27;
+
+Console.WriteLine(myName);
