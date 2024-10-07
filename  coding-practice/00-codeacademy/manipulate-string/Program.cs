5ab110f82e947bfabb08b7f465396a69435f771e
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-10-07 18:40:15 +0900

    add:
    コーディング演習の追加

---  coding-practice/00-codeacademy/manipulate-string/Program.cs
+++  coding-practice/00-codeacademy/manipulate-string/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Script line
+string script = "Close on a portrait of the HANDSOME PRINCE -- as the BEAST'S giant paw slashes it.";
+
+// Get camera directions
+int charPosition = script.IndexOf("Close");
+int length = "Close on".Length;
+string cameraDirections = script.Substring(charPosition, length);
+
+// Get scene description
+charPosition = script.IndexOf("a portrait");
+string sceneDescription = script.Substring(charPosition);
+
+// cameraDirections大文字にして、変数に再保存
+cameraDirections = cameraDirections.ToUpper();
+
+// sceneDescription小文字にして変数に再保存
+sceneDescription = sceneDescription.ToLower();
+
+// 結果をコンソールに出力
+System.Console.WriteLine(cameraDirections);
+System.Console.WriteLine(sceneDescription);
