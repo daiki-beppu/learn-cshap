8ab2e6eb44c83709f4d2b2663f9650146b8f3192
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-10-08 20:59:58 +0900

    add:
    2024/10/8
    コーディング演習の追加

---
+++  coding-practice/00-codeacademy/boolean-data-types/Program.cs
@@ -0,0 +1,6 @@
+﻿
+// 500 は 24 より大きいか？
+bool answerOne = true;
+
+// coffee に ｢a｣ は含まれるか？
+bool answerTwo = false;
---
+++  coding-practice/00-codeacademy/equality-operators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+int cookiesSoldGuess = 250;
+
+// クッキーの販売数を初期化
+int storeOneCookies = 110;
+int storeTwoCookies = 135;
+
+// クッキーの販売数の合計
+int cookiesSoldActual = storeOneCookies + storeTwoCookies;
+
+// 販売されたクッキーの合計数を正しく推測したかを表す
+bool guessedCorrect = cookiesSoldGuess == cookiesSoldActual;
+
+// 結果をコンソールに出力
+System.Console.WriteLine(guessedCorrect);
