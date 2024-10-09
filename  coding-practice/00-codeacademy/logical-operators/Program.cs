fb17bf90059c1a36e5c4dcd65028a2bf8e05dc4d
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-10-09 11:05:44 +0900

    add:
    2024/10/9
    コーディング演習の追加

---
+++  coding-practice/00-codeacademy/logical-operators/Program.cs
@@ -0,0 +1,17 @@
+﻿// 論理演算子
+
+bool beach = true;
+bool hiking = false;
+bool city = true;
+
+// bool 型で yourNeeds という名前の変数を作成し値は beach かつ city
+bool yourNeeds = beach && city;
+
+// bool 型で friendNeeds という名前の変数を作成し値は beach または haiking
+bool friendNeeds = beach || hiking ;
+
+// yourNeeds と friendNeeds を比較し tripDecision に保存
+bool tripDecision = yourNeeds && friendNeeds;
+
+// 結果をコンソールに出力
+System.Console.WriteLine(tripDecision);
