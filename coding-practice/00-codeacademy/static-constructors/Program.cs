84e6d15ef3cf076f98e2dbe500b438cccc1737f0
daiki_beppu (別府 大貴) <beppu.engineer@gmail.com>
2024-10-15 16:04:55 +0900

    add:
    2024/10/15
    コーディング演習の追加

---
+++ coding-practice/00-codeacademy/static-constructors/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+これまでの練習では、forestsCreatedフィールドとforestFactsフィールドに開始値が与えられていませんでした！ まず、Forestの静的コンストラクタを作成します。
+
+静的コンストラクタの本体で、forestFactsフィールドにこの文字列を設定する：
+
+また、静的コンストラクタの本体内で、ForestsCreatedプロパティを0に設定する。
+
+Program.cs で、Forest.PrintForestFacts() を呼び出して、ForestFacts プロパティが設定されていることを確認します。
+*/
+
+using System;
+
+namespace StaticConstructors
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Forest.PrintForestFacts();
+    }
+  }
+}
